54faf21f1ee18f810bedd64e0b03200a9afa29c2
Astthor <a.bragason@gmail.com>
2022-05-31 14:43:45 +0200

    race to all, and return random of the three

--- src/database-service/default-db/stock.default-db.service.ts
+++ src/database-service/default-db/stock.default-db.service.ts
@@ -2,27 +2,31 @@ import { InterfaceStockDatabaseService } from "../interface-stock.database-servi
 import { StockValue } from "../../models/dto/stock-value.dto";
 import { stockNeo4jService } from "../neo4j/stock.neo4j.service";
 import { stockMongodbService } from "../mongodb/stock.mongodb.service";
+import { randomIntFromInterval } from "../../utils/random";
 
 export const stockDefaultDbService: InterfaceStockDatabaseService = {
-  createStock(stockValue: StockValue): Promise<StockValue | null> {
-    return Promise.race([
+  async createStock(stockValue: StockValue): Promise<StockValue | null> {
+    const stock = await Promise.all([
       stockNeo4jService.createStock(stockValue),
       stockMongodbService.createStock(stockValue),
       stockMongodbService.createStock(stockValue),
     ]);
+		return stock[randomIntFromInterval(0, 2)];
   },
-  getStock(stockTicker: string): Promise<StockValue | null> {
-    return Promise.race([
+  async getStock(stockTicker: string): Promise<StockValue | null> {
+    const stock = await Promise.all<StockValue|null> ([
       stockNeo4jService.getStock(stockTicker),
       stockMongodbService.getStock(stockTicker),
       stockMongodbService.getStock(stockTicker),
     ]);
+		return stock[randomIntFromInterval(0, 2)];
   },
-  updateStock(currentStockValue: StockValue): Promise<StockValue | null> {
-    return Promise.race([
+  async updateStock(currentStockValue: StockValue): Promise<StockValue | null> {
+    const stock = await Promise.all([
       stockNeo4jService.updateStock(currentStockValue),
       stockMongodbService.updateStock(currentStockValue),
       stockMongodbService.updateStock(currentStockValue),
     ]);
+		return stock[randomIntFromInterval(0, 2)];
   },
 };
